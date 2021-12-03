cb0dcba89843c16aed2edd3d01035f241cb6c6f1
Andrei <andreigabor36@gmail.com>
2021-12-03 21:06:07 +0200

    working on android

---
+++ Android/Client/MyApp2/app/src/main/java/com/ilazar/myapp2/todo/individuals/CarsViewModel.kt
@@ -0,0 +1,48 @@
+package com.ilazar.myapp2.todo.cars
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ilazar.myapp2.todo.data.CarRepository
+import com.ilazar.myapp2.core.TAG
+import com.ilazar.myapp2.core.Result
+import com.ilazar.myapp2.todo.data.Car
+import kotlinx.coroutines.launch
+
+class CarListViewModel : ViewModel() {
+    private val mutableCars = MutableLiveData<List<Car>>().apply { value = emptyList() }
+    private val mutableLoading = MutableLiveData<Boolean>().apply { value = false }
+    private val mutableException = MutableLiveData<Exception>().apply { value = null }
+
+    val cars: LiveData<List<Car>> = mutableCars
+    val loading: LiveData<Boolean> = mutableLoading
+    val loadingError: LiveData<Exception> = mutableException
+
+    fun createCar(position: Int): Unit {
+        val list = mutableListOf<Car>()
+        list.addAll(mutableCars.value!!)
+        list.add(Car(position.toString(), "Car " + position))
+        mutableCars.value = list
+    }
+
+    fun loadCars() {
+        viewModelScope.launch {
+            Log.v(TAG, "loadCars...");
+            mutableLoading.value = true
+            mutableException.value = null
+            when (val result = CarRepository.loadAll()) {
+                is Result.Success -> {
+                    Log.d(TAG, "loadCars succeeded");
+                    mutableCars.value = result.data
+                }
+                is Result.Error -> {
+                    Log.w(TAG, "loadCars failed", result.exception);
+                    mutableException.value = result.exception
+                }
+            }
+            mutableLoading.value = false
+        }
+    }
+}
